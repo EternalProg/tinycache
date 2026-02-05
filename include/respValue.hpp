023eed94c5f3ce67c6a948a40a1bb18e15dc255d
EternalProg <eternal.proglife@gmail.com>
2026-02-05 16:14:59 +0200

    add constructors

--- include/respValue.hpp
+++ include/respValue.hpp
@@ -20,8 +20,26 @@ struct RespValue {
     kNullBulkString,
     kArray,
   };
-
   using RespArray = std::vector<RespValue>;
+
+  RespValue() = default;
+  RespValue(const RespValue&) = default;
+  RespValue(RespValue&&) noexcept = default;
+  RespValue& operator=(const RespValue&) = default;
+  RespValue& operator=(RespValue&&) noexcept = default;
+  ~RespValue() = default;
+
+  RespValue(Type t, std::string str) : type(t), data(std::move(str)) {}
+
+  RespValue(Type t, std::int64_t integer) : type(t), data(integer) {}
+
+  RespValue(Type t, RespArray array) : type(t), data(std::move(array)) {}
+
+  explicit RespValue(std::int64_t integer)
+      : type(Type::kInteger), data(integer) {}
+
+  explicit RespValue(RespArray array)
+      : type(Type::kArray), data(std::move(array)) {}
 
   Type type = RespValue::Type::kUnknown;
   std::variant<std::string,   // Simple / Error / Bulk
