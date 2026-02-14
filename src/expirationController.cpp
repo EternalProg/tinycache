362be88f2d37063b531dd063cc0c69ad6302379c
EternalProg <eternal.proglife@gmail.com>
2026-02-14 19:14:46 +0200

    improve cleaning_loop implementation to use the same timer

--- src/expirationController.cpp
+++ src/expirationController.cpp
@@ -8,6 +8,9 @@ ExpirationController::ExpirationController(LruCache& cache) : cache_(cache) {
 }
 
 asio::awaitable<void> ExpirationController::cleaning_loop() {
+  auto executor = co_await asio::this_coro::executor;
+  asio::steady_timer timer(executor);
+
   while (true) {
     auto next_expire_time = cache_.get_next_expire_time();
 
@@ -15,19 +18,13 @@ asio::awaitable<void> ExpirationController::cleaning_loop() {
       auto now = std::chrono::steady_clock::now();
 
       if (next_expire_time.value() > now) {
-        auto wait_duration =
-            std::chrono::duration_cast<std::chrono::milliseconds>(
-                next_expire_time.value() - now);
-
-        co_await asio::steady_timer(co_await asio::this_coro::executor,
-                                    wait_duration)
-            .async_wait(asio::use_awaitable);
+        timer.expires_at(next_expire_time.value());
+        co_await timer.async_wait(asio::use_awaitable);
       }
     } else {
       // No keys with expiration, wait for a fixed interval before checking again
-      co_await asio::steady_timer(co_await asio::this_coro::executor,
-                                  std::chrono::seconds(3))
-          .async_wait(asio::use_awaitable);
+      timer.expires_after(std::chrono::seconds(3));
+      co_await timer.async_wait(asio::use_awaitable);
     }
 
     cache_.remove_expired_keys(std::chrono::steady_clock::now());
